5160aa804c02d99950425d1033096e67f79c55f9
N Tyan <princeenvyanima@gmail.com>
2022-01-11 22:19:17 +0300

    delete logs2

--- resources/views/order/executors.blade.php
+++ resources/views/order/executors.blade.php
@@ -6,35 +6,39 @@
             <div class="album py-5 bg-light">
                 <div class="container">
                     <div class="row">
+                    @isset($orgs)
+                            @foreach($orgs as $org)
+                                <div class="col-md-4">
+                                    <div class="card mb-4 box-shadow">
+                                        <div class="card-img-top "
+                                             style="max-height: 200px; height:180px;
+                                                 background:url({{$path[$org->id]}}) no-repeat center; background-size: contain">
 
-                        @foreach($orgs as $org)
-                        <div class="col-md-4">
-                            <div class="card mb-4 box-shadow">
-                                <div class="card-img-top "
-                                     style="max-height: 200px; height:180px;
-                                         background:url({{$path[$org->id]}}) no-repeat center; background-size: contain">
-
-                                </div>
-                                <div class="card-body">
-                                    <h4>{{$org->name}}</h4>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a href="/organization/{{$org->id}}" class="btn btn-sm btn-outline-secondary">Просмотр</a>
-                                            <a href="/executor/{{$org->id}}" class="btn btn-sm btn-outline-primary">Выбрать</a>
                                         </div>
-                                        <small class="text-muted">
-                                            @isset($prices[$org->id])
-                                                Цены от <b>{{min(($prices[$org->id]))}}</b>р./г.
-                                            @endisset
-                                            @empty($prices[$org->id])
-                                                Цены не указаны
-                                            @endempty
-                                        </small>
+                                        <div class="card-body">
+                                            <h4>{{$org->name}}</h4>
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <div class="btn-group">
+                                                    <a href="/organization/{{$org->id}}" class="btn btn-sm btn-outline-secondary">Просмотр</a>
+                                                    <a href="/executor/{{$org->id}}" class="btn btn-sm btn-outline-primary">Выбрать</a>
+                                                </div>
+                                                <small class="text-muted">
+                                                    @isset($prices[$org->id])
+                                                        Цены от <b>{{min(($prices[$org->id]))}}</b>р./г.
+                                                    @endisset
+                                                    @empty($prices[$org->id])
+                                                        Цены не указаны
+                                                    @endempty
+                                                </small>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        @endforeach
+                            @endforeach
+                    @endisset
+                    @empty($orgs)
+                            <p class="text-muted mx-3">Подходящих исполнителей не найдено</p>
+                    @endempty
 
                     </div>
                 </div>
